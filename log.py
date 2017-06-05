769999654aafe0a7d56973d6d3c98e20d18e8a2a
Daniel Laurence <daniellaurence269@gmail.com>
2017-06-05 13:24:57 -0400

    Host web page working

--- log.py
+++ log.py
@@ -1,6 +1,16 @@
-import log.html # connect the HTML page display with this python page
-import log.sql # connect the SQL queries with this python page
-import cgi # import the library that allows for escaping
+from flask import render_template
+from flask import Flask, request, redirect, url_for
 
-def espcape_html(s): # escaping should not be necessary becasue there should be no imputs, simply the display of the results, but just in case
-    return cgi.escape(s, quote = True)
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def main():
+  '''Main page of the results.'''
+  # results = "".join(POST % (date, text) for text, date in get_results())
+  html = render_template('log.html', popular_articles='substitution_string_here', 
+  									 popular_authors='substitution_string_here', 
+  									 high_errors='substitution_string_here' ) #% results
+  return html
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080, debug=True)
--- log.sql
+++ log.sql
@@ -1,11 +1,64 @@
 # make sure to answer the three questions, with only one SQL query each:
-# 1. What are the most popular three articles of all time? -article name and number of views of the cooresponding articles
-# 2. Who are the most popular article authors of all time? -author name and number of views of all their articles
-# 3. On which days did more than 1% of requests lead to errors? -date and percent errors
+	# 1. What are the most popular three articles of all time?
+		# article name and number of views of the cooresponding articles
+	# 2. Who are the most popular article authors of all time?
+		# author name and number of views of all their articles
+	# 3. On which days did more than 1% of requests lead to errors?
+		# date and percent errors
 
-# result1
+# popular_articles
 	# article name
-	SELECT name FROM authors limit 5;
+	##### SELECT name FROM authors limit 5;
 	# number of views
-	SELECT max(path) FROM log group by path order by count(path) desc;
+	##### SELECT max(path) FROM log group by path order by count(path) desc;
 	# join
+	#####
+
+# popular_authors
+	# author name
+	##### select name from authors
+	# number of views of all their articles
+	#####
+	# join
+	#####
+
+# high_errors
+	# date
+	#####
+	# percent errors
+	#####
+	# join
+	#####
+
+#forumdb equivalent from previous project
+
+import psycopg2, bleach
+
+DBNAME = "news"
+
+def get_popular_articles():
+  """ Return 'What are the most popular three articles of all time?' """
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("SELECT name FROM authors limit 5;")
+  popular_articles = c.fetchall()
+  db.close()
+  return popular_articles
+
+  def get_popular_authors():
+  """ Return 'Who are the most popular article authors of all time?' """
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("SELECT name FROM authors limit 5;")
+  popular_authors = c.fetchall()
+  db.close()
+  return popular_authors
+
+  def get_high_errors():
+  """ Return 'On which days did more than 1% of requests lead to errors?' """
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("SELECT name FROM authors limit 5;")
+  high_errors = c.fetchall()
+  db.close()
+  return high_errors
