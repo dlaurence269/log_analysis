962eaba1115524985ada326dce87fb40dc9ebecc
Daniel Laurence <daniellaurence269@gmail.com>
2017-06-06 17:09:12 -0400

    popular_articles query working

--- log.sql
+++ log.sql
@@ -2,7 +2,7 @@
 	
 	# popular_articles
 		# 1. What are the most popular three articles of all time?
-			# article name and number of views of the cooresponding articles
+			# article name and number of views of the corresponding articles
 	
 	# popular_authors
 		# 2. Who are the most popular article authors of all time?
@@ -15,43 +15,32 @@
 # popular_articles
 	# articles.author = authors.id
 	# articles.slug = log.path OR rather articles./articles/slug = log.path
-	# articles.title, and number_of_views needs to be the final information shown in order from greatest to least limit 3
-	# 
+	# articles.title, and number_of_views needs to be the result in order from greatest to least limit 3
 
 	select title from articles order by id desc limit 3; # to show the article name in order, need to substitute id for number_of_views
-	select concat ('/articles/', slug) from articles limit 3; # concatenate articles.slug so it can join with log.path
+	select concat ('/article/', slug) from articles limit 3; # concatenate articles.slug so it can join with log.path
 	select path, count(path) from log group by path order by count(path) desc limit 3; # number_of_views
-	select  # final join product
 
-	select articles.title from articles join log on articles.concat('/articles/', slug) = log.path order by id desc limit 3;
-
-
-
-	SELECT articles*, authors*, log*
-	FROM articles
-    JOIN authors.id = articles.author
-    JOIN log
-        ON log.path = articles.concat('/articles/',slug)
-	WHERE DATE(TableC.date)=date(now())
-	order by count(path) desc limit 3;
-
-	OR
-
-	SELECT 
-    a.nameA, /* TableA.nameA */
-    d.nameD /* TableD.nameD */
-	FROM TableA a 
-    INNER JOIN TableB b on b.aID = a.aID 
-    INNER JOIN TableC c on c.cID = b.cID 
-    INNER JOIN TableD d on d.dID = a.dID 
-	WHERE DATE(c.`date`) = CURDATE()
+	select title, count(title) as number_of_views
+	from log 
+		join articles on concat('/article/', slug) = path 
+	group by title
+	order by count(title) desc
+	limit 3;
 
 # popular_authors
 	# articles.author = authors.id
 	# articles.slug = log.path OR rather articles./articles/slug = log.path
+	# author.name, and number_of_total_views to be in the result in order from greatest to least limit 3
 
-
-	join # final join product
+	select name, count(name) as number_of_total_views
+	from log 
+		join articles on concat('/article/', slug) = path 
+	from authors
+		join articles on authors.name = articles.id
+	group by name
+	order by count(name) desc
+	limit 3;
 
 # high_errors
 	# date
